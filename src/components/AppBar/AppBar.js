d775d97c2fac0a60f970a380ed9063aeb3e362fd
YuriiStreltsov <65507638+YuriiStreltsov@users.noreply.github.com>
2021-02-06 13:24:41 +0200

    Refactoring AppBar and remove NavHeader component

--- src/components/AppBar/AppBar.js
+++ src/components/AppBar/AppBar.js
@@ -1,9 +1,23 @@
-import NavHeader from '../NavHeader/NavHeader';
+import { NavLink } from 'react-router-dom';
+import routes from '../../routes';
 import './AppBar.scss';
 
 const AppBar = () => (
   <header className="AppBar">
-    <NavHeader />
+    <nav className="navigation">
+      <NavLink
+        exact
+        to={routes.home}
+        className="link"
+        activeClassName="activeLink"
+      >
+        Home
+      </NavLink>
+
+      <NavLink to={routes.movies} className="link" activeClassName="activeLink">
+        Movies
+      </NavLink>
+    </nav>
   </header>
 );
 
