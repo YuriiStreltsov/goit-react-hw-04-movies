c01004f62f1be6321171d8fac17b4e76a68c9566
Yurii <streltsovyurii86@gmal.com>
2021-02-07 20:03:34 +0200

    Add YEAR to the title of the movie

--- src/components/MovieInfo/MainInfo/MovieMainInfo.js
+++ src/components/MovieInfo/MainInfo/MovieMainInfo.js
@@ -3,12 +3,20 @@ import originPath from '../../../path';
 import './MovieMainInfo.scss';
 
 const MovieMainInfo = ({ movie, defaultImg }) => {
-  const { title, poster_path, vote_average, overview, genres } = movie;
+  const {
+    title,
+    poster_path,
+    vote_average,
+    overview,
+    genres,
+    release_date,
+  } = movie;
 
   const userScore = vote_average * 10;
 
   const imgPath = `${originPath.imagePoster}${poster_path}`;
   const imgUrl = poster_path ? imgPath : defaultImg;
+  const releaseYear = release_date.slice(0, 4);
 
   return (
     <>
@@ -18,7 +26,9 @@ const MovieMainInfo = ({ movie, defaultImg }) => {
         </div>
 
         <div className="MovieDesc">
-          <h2 className="title">{title}</h2>
+          <h2 className="title">
+            {title}({releaseYear})
+          </h2>
           <p className="text">User score: {userScore} %</p>
           <h3>Overview</h3>
           {overview ? <p>{overview}</p> : <p>No data</p>}
